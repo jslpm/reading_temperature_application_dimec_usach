2359dd38fe6218cbca102b089cf11978c434e01b
Jose Pascal <jose.pascal@gmail.com>
2022-11-23 22:24:05 -0300

    Add simple gui for testing

---
+++ gui/simple_gui.py
@@ -0,0 +1,88 @@
+import nidaqmx
+import sys
+
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QPushButton, QLineEdit,
+    QFormLayout, QHBoxLayout, QMessageBox)
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
+
+class DAQApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initializeUI()
+
+    def initializeUI(self):
+        """
+        Intialize the windows and display its content to the screen.
+        """
+        self.setGeometry(100, 100, 400, 400)
+        self.setWindowTitle('DAQ Application')
+        self.formWidgets()
+        self.show()
+
+    def formWidgets(self):
+        """
+        Create widgets that will be used in the application form.
+        """        
+        device_name = QLineEdit()
+        device_name.setText('cDAQ1Mod1/ai0')
+        
+        self.connect_button = QPushButton('Connect')
+        self.connect_button.clicked.connect(self.connectDAQ)
+
+        self.read_button = QPushButton('Read value')
+        self.read_button.setEnabled(False)
+        self.read_button.clicked.connect(self.read_value)
+
+        self.data_label = QLabel()
+        
+        # Create form layout
+        app_form_layout = QFormLayout()
+        app_form_layout.addRow('Device', device_name)
+        app_form_layout.addRow(self.connect_button)
+        app_form_layout.addRow(self.read_button, self.data_label)
+
+        self.setLayout(app_form_layout)
+
+    def connectDAQ(self):
+        try:
+            self.task = nidaqmx.Task()
+            self.task.ai_channels.add_ai_thrmcpl_chan(
+                'cDAQ1Mod1/ai0', 
+                units=nidaqmx.constants.TemperatureUnits.DEG_C,
+                thermocouple_type=nidaqmx.constants.ThermocoupleType.K
+            )
+            self.task.timing.cfg_samp_clk_timing(1)
+        except:
+            raise Exception("Cannot create task for accesing DAQ. Check DAQ connection.")
+        else:
+            print('Connected to DAQ.')
+            self.connect_button.setEnabled(False)
+            self.read_button.setEnabled(True)
+
+    def read_value(self):
+        measurement = self.task.read()
+        self.data_label.setText(str(measurement))
+
+    def closeEvent(self, event):
+        close_question = QMessageBox.question(
+            self, 
+            'Close program', 
+            'Do you want to close the application?', 
+            QMessageBox.Yes | QMessageBox.No, 
+            QMessageBox.No
+        )
+
+        if close_question == QMessageBox.Yes:
+            self.task.close()
+            event.accept()
+        else:
+            event.ignore()
+        
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = DAQApp()
+    sys.exit(app.exec_())
